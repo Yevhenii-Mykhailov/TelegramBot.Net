14d741344c76cce8162aefac4a4f92c5bc0dfad3
Yevhenii <mihaylovskiyeugeny@gmail.com>
2021-07-24 16:54:36 +0300

    moved the methods into a separate class
    moved the methods into a separate class

--- CustomControls/BotCommands.cs
+++ CustomControls/BotCommands.cs
@@ -6,7 +6,36 @@ namespace TelegramBot
 {
     public class BotCommands : BotHelper
     {
-        
+        [Obsolete]
+        public void SendUserLocation(object sender, MessageEventArgs e)
+        {
+
+            if (e.Message.Location != null)
+            {
+                string userLatitude = Convert.ToString(e.Message.Location.Latitude);
+                string userLongitude = Convert.ToString(e.Message.Location.Longitude);
+
+                BotClient.SendTextMessageAsync(
+                chatId: e.Message.Chat,
+                text: $"Your Location is: Latitude:{userLatitude}; Longitude:{userLongitude}"
+                );
+            }
+        }
+
+        [Obsolete]
+        public void SendUserFullName(object sender, MessageEventArgs e)
+        {
+            string userFirstName = e.Message.From.FirstName;
+            string userLastName = e.Message.From.LastName;
+
+            if (e.Message.Text != null)
+            {
+                BotClient.SendTextMessageAsync(
+                chatId: e.Message.Chat,
+                text: $"Your name is {userFirstName} {userLastName}"
+                );
+            }
+        }
 
     }
 }
